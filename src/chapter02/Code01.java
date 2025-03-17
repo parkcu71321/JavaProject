e1497f2985acd18e1bec3344197f6865ac2f12bb
parkcu71321 <parkcu71321@gmail.com>
2025-03-17 11:52:18 +0900

    변경

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args){
+
+        System.out.println("hi");
+        System.out.print("박찬웅\n");      //      /n : 줄바꿈
+        System.out.println("줄바꿈");
+        System.out.printf("변수 형식을 지정해서 출력하고자 할 때 사용");
+        System.out.println(20+20+"결과");
+        System.out.println("결과"+20+20); //문자와 숫자가 만나면 +는 연결연산자로 변함 <과정>1.결과(문자) + 20 2. (결과 + 20)(문자로 봄) +20
+    }
+}
