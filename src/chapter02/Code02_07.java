87a2d08458649b9dd97b442cca35778ae466e3c6
parkcu71321 <parkcu71321@gmail.com>
2025-03-24 10:44:06 +0900

    사칙계산

---
+++ src/chapter02/Code02_07.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+public class Code02_07 {
+    public static void main(String[]args){
+
+        int a=100;
+        int b=50;
+
+        int result=a+b;
+        System.out.println(result);                 //덧셈
+
+        int result1=a-b;
+        System.out.println(result1);                 //뺄셈
+
+        int result2=a*b;
+        System.out.println(result2);                 //곱셈
+
+        int result3=a/b;
+        System.out.println(result3);                 //나눗셈
+    }
+}
