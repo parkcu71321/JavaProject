8c2799e5c1a49575756b87c620b12311d9487c9f
parkcu71321 <parkcu71321@gmail.com>
2025-03-24 11:36:06 +0900

    문자여열

--- src/chapter02/Code02_08.java
+++ src/chapter02/Code02_08.java
@@ -2,7 +2,7 @@ package chapter02;
 
 public class Code02_08 {
     public static void main(String[] args){
-        String str1="난생처음";             //main 메서드 안에서만 쓸 수 있는 지역변수임
+        String str1="난생처음";             //main 메서드 안에서만 쓸 수 있는 지역변수임, 지역 변수는 초기화 시켜야함
         String str2="자바";
         System.out.println(str1+str2);
         String result=str1+str2;
