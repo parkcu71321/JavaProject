f25045777dfb43420256ac45ea7e8b844c4a25bd
parkcu71321 <parkcu71321@gmail.com>
2025-03-24 11:09:01 +0900

    문자여열

---
+++ src/chapter02/Code02_08.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Code02_08 {
+    public static void main(String[] args){
+        String str1="난생처음";             //main 메서드 안에서만 쓸 수 있는 지역변수임
+        String str2="자바";
+        System.out.println(str1+str2);
+        String result=str1+str2;
+        System.out.println(result);
+    }
+}
